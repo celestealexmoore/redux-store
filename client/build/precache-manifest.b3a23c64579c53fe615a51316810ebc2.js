cea8804191b4f1fb53d06a68346336234f722146
Celeste Moore <celestealexmoore@gmail.com>
2021-08-06 13:17:57 -0500

    updating

---
+++ client/build/precache-manifest.b3a23c64579c53fe615a51316810ebc2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f773f4743a74f423e2ba0b68283a3669",
+    "url": "/index.html"
+  },
+  {
+    "revision": "24e91d4466c4c8419929",
+    "url": "/static/css/main.a64bd9e7.chunk.css"
+  },
+  {
+    "revision": "7fe85ccd4cf1fb63ec51",
+    "url": "/static/js/2.25026a5d.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.25026a5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24e91d4466c4c8419929",
+    "url": "/static/js/main.d84452d7.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
